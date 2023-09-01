d4e2e451cfecf933ab37b0f03f3ae7d684817649
홍준성 <jshong@sdt.inc>
2023-09-01 19:12:38 +0900

    git init

---
+++ src/main/java/com/example/zipkintest/ZipkinTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.zipkintest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinTestApplication.class, args);
+    }
+
+}
